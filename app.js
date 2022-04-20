03d32375b47ca19a2a30c7f5f7db6fbdae661af0
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-20 10:12:50 -0700

    se agrego una nueva ruta en la que lo que se ponga en la url sera lo que recibira para luego desplegar

--- app.js
+++ app.js
@@ -20,7 +20,13 @@ app.get('/launchx', (req, res) => {
 //otro path on un objeto dentro
 app.get('/objectnodejs', (req, res) => {
     const exp = {name: "Abraham Ariel Arizmendi Grijalva", msg: "wolis"}
-    res.send(exp)
+    res.send(exp) //response
+})
+
+//nuevp path
+//la ruta es explorers, pero lo que se ponga despues de exploeres en la ruta (url) sera lo que se guarde en "explorerName" y eso sera lo que se vera en la pagina
+app.get('/explorers/:explorerName', (req, res) => {
+    res.send(req.params) //require parameters
 })
 
 //inicializamos la app
